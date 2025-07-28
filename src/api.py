57260b054f0768acb5764094c1aebc5e5bce05e8
e0406370 <e0406370@u.nus.edu>
2025-07-28 20:33:33 +0800

    [chore] add cron job to prevent idling

--- src/api.py
+++ src/api.py
@@ -1,6 +1,9 @@
+from aiocron import crontab
 from datetime import datetime, timezone
 from fastapi import FastAPI, Response, status
 from fastapi.middleware.cors import CORSMiddleware
+from httpx import AsyncClient
+from os import environ
 from src.lib import Logger, MsgSpecJSONResponse
 from src.search import search_dramas
 from typing import Any, Dict
@@ -38,10 +41,20 @@ def search(query: str, response: Response) -> Dict[str, Any]:
             "scrape_date": datetime.now(timezone.utc).isoformat(),
         }
 
-    except Exception as e:
+    except Exception:
         Logger.exception("Failed to search dramas.")
 
         response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
         return {
             "message": "An unexpected error occurred."
         }
+        
+@crontab("*/15 * * * *")
+async def heartbeat():
+    try:
+        async with AsyncClient() as client:
+            await client.get(environ.get("BASE"))
+            Logger.info("Self-ping succeeded.")
+
+    except Exception:
+        Logger.exception("Self-ping failed.")
