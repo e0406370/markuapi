3b24a3a6d3099989058e38215c6df98d58c1032c
e0406370 <e0406370@u.nus.edu>
2025-08-02 01:01:31 +0800

    [feat] include more fields in search EP
    mark_count, clip_count, series_id, season_id

--- src/search.py
+++ src/search.py
@@ -19,7 +19,9 @@ def search_dramas(query: str) -> List[Dict[str, str]]:
         Logger.warn(f"[0 | Query: {query}] 一致する情報は見つかりませんでした。")
         return dramas
 
-    results = soup.find("div", class_="p-contents-grid").find_all(
+    results = soup.find(
+        "div", class_="p-contents-grid"
+    ).find_all(
         "div", class_="js-cassette"
     )
 
@@ -27,13 +29,21 @@ def search_dramas(query: str) -> List[Dict[str, str]]:
         title_elem = result.find("h3", class_="p-content-cassette__title")
         rating_elem = result.find("div", class_="c-rating__score")
 
-        data_elem = MsgSpecJSONResponse.parse(result.attrs["data-clip"])
-        drama_series_id = data_elem["drama_series_id"]
-        drama_season_id = data_elem["drama_season_id"]
+        data_mark_elem = MsgSpecJSONResponse.parse(result.attrs["data-mark"])
+        mark_count = data_mark_elem["count"]
+
+        data_clip_elem = MsgSpecJSONResponse.parse(result.attrs["data-clip"])
+        drama_series_id = data_clip_elem["drama_series_id"]
+        drama_season_id = data_clip_elem["drama_season_id"]
+        clip_count = data_clip_elem["count"]
 
         d = {}
         d["title"] = title_elem.string
         d["rating"] = rating_elem.string
+        d["mark_count"] = mark_count
+        d["clip_count"] = clip_count
+        d["series_id"] = drama_series_id
+        d["season_id"] = drama_season_id
         d["link"] = FILMARKS_DRAMA.format(drama_series_id=drama_series_id, drama_season_id=drama_season_id)
 
         Logger.info(f"[{ctr + 1} | Query: {query}] {str(d)}")
--- tests/test_api.py
+++ tests/test_api.py
@@ -26,6 +26,10 @@ def test_invalid_endpoint() -> None:
         {
             "title": "あなたの番です",
             "rating": 4.0,
+            "mark_count": 29975,
+            "clip_count": 6060,
+            "series_id": 6055,
+            "season_id": 8586,
             "link": "https://filmarks.com/dramas/6055/8586",
         },
     ],
@@ -38,6 +42,10 @@ def test_search_with_results(test_data) -> None:
     assert resp.status_code == 200
     assert resp_dramas[0]["title"] == test_data["title"]
     assert float(resp_dramas[0]["rating"]) == pytest.approx(test_data["rating"], abs=0.5)
+    assert resp_dramas[0]["mark_count"] == pytest.approx(test_data["mark_count"], abs=1000)
+    assert resp_dramas[0]["clip_count"] == pytest.approx(test_data["clip_count"], abs=1000)
+    assert resp_dramas[0]["series_id"] == test_data["series_id"]
+    assert resp_dramas[0]["season_id"] == test_data["season_id"]
     assert resp_dramas[0]["link"] == test_data["link"]
 
 
